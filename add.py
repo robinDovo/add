311f4af7e7479b654f43aebb0beb959742838356
robin Dovo <m104258979@gmail.com>
2020-02-04 21:05:06 +0800

    basic

---
+++ add.py
@@ -0,0 +1,15 @@
+password = '123456'
+
+i = 3
+
+while True:
+    pwd = input('請輸入密碼！')
+    if pwd == password:
+    	print('登入成功')
+    	break
+    else:
+    	i = i - 1
+    	print('登入失敗剩餘',i,'幾次機會！')
+    	
+    	if i == 0:
+    		break
